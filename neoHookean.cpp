b699bdd98cb97397949d17cae5592b6e7df7daa5
Alejandro <76975391+aerodriguezsanchez@users.noreply.github.com>
2021-10-08 20:56:52 -0500

    Add files via upload

---
+++ neoHookean.cpp
@@ -0,0 +1,70 @@
+/* © 2020 Alejandro E. Rodriguez.  All rights reserved. //For personal use only.
+It is not permitted to distribute verbatim copies of this document,
+and changing it is not allowed. Contact the owner of the code at arodsn.com blog
+for further permissions.*/
+
+#include <math.h>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+std::vector<double> neoHookean(std::vector<double> trueStrain, double mu)
+{
+	std::vector<double> stress(trueStrain.size());
+	std::vector<double> lam(trueStrain.size());
+	
+	for (int i = 0; i < trueStrain.size(); i++)
+	{
+		lam[i] = exp(trueStrain[i]);
+	}
+
+	//Tensile
+	for (int i = 1; i < trueStrain.size(); i++)
+	{
+		stress[i] += mu*(lam[i]*lam[i]-1/lam[i]);
+	}
+	return stress;
+}
+
+void main() {
+
+	ofstream Sigma;
+	Sigma.open("Sigma.csv");
+	ofstream Epsilon;
+	Epsilon.open("Epsilon.csv");
+
+	std::vector<double> trueStrain(100);
+	for (int i = 0; i < trueStrain.size(); i++) {
+		trueStrain[i] = (0.022) * i;
+	}
+
+	std::vector<double> trueStress(trueStrain.size());
+	double mu = 0.406758;
+
+	double matrix[100][2]; ///
+
+	trueStress = neoHookean(trueStrain, mu);
+
+	for (int i = 0; i < trueStrain.size(); i++) {
+		Epsilon << trueStrain[i] << "\n";
+		Sigma << trueStress[i] << "\n";
+
+		///------------------------- Test to see if a matrix can be saved into a CSV file
+		matrix[i][0] = exp(trueStrain[i]) - 1; //From true strain to engineering strain
+		matrix[i][1] = (trueStress[i]) / exp(trueStrain[i]); // From true stress to engineering stress
+	}
+	///------------------------- Test to see if a matrix can be saved into a CSV file
+
+	ofstream Matrix;
+	Matrix.open("Matrix.csv");
+
+	for (auto& row : matrix) {
+		for (auto col : row)
+			Matrix << col << ',';
+		Matrix << '\n';
+	}
+
+	Epsilon.close();
+	Sigma.close();
+	Matrix.close();
+}
